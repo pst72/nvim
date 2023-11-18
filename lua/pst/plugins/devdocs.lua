62d6a3d9e8ea23371eb9f93ed61dea8d3ee86ef2
pst72 <pstocks1@gmail.com>
2023-11-18 14:26:36 +0200

    got devdocs running

--- lua/pst/plugins/devdocs.lua
+++ lua/pst/plugins/devdocs.lua
@@ -21,6 +21,9 @@ return {
   --   { '<localleader>dF', '<cmd>DevdocsOpenCurrentFloat<cr>', desc = 'Open Current filetype DevDocs in Float ' },
   --   { '<localleader>du', '<cmd>DevdocsUpdate<cr>', desc = 'Update DevDocs ' },
   -- },
+  enabled = true,
+  lazy = true,
+  cmd = { "DevdocsInstall", "DevdocsUninstall", "DevdocsOpen", "DevdocsOpenFloat", "DevdocsOpenCurrent", "DevdocsOpenCurrentFloat", "DevdocsUpdate" },
   opts = {
     dir_path = vim.fn.stdpath("data") .. "/devdocs", -- installation directory
     telescope = {}, -- passed to the telescope picker
@@ -35,12 +38,13 @@ return {
     cmd_args = {'s', 'dark', 'w', '80'}, -- example using glow: { "-s", "dark", "-w", "80" }
     cmd_ignore = {}, -- ignore cmd rendering for the listed docs
     picker_cmd = true, -- use cmd previewer in picker preview
-    picker_cmd_args = {'s', 'dark', 'w', '50'}, -- example using glow: { "-s", "dark", "-w", "50" }
+    picker_cmd_args = {'s', 'dark', 'w', '80'}, -- example using glow: { "-s", "dark", "-w", "50" }
     mappings = { -- keymaps for the doc buffer
       open_in_browser = ""
     },
     ensure_installed = {}, -- get automatically installed
     after_open = function(bufnr)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'q', ':close<CR>', {})
       vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Esc>', ':close<CR>', {})
     end, -- callback that runs after the Devdocs window is opened. Devdocs buffer ID will be passed in
   }
